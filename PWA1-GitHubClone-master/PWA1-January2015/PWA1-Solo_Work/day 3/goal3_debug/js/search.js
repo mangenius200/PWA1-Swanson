0e69dd72e924f504af7bdc8f4d6e6e3a6e61d44b
mangenius200 <rashaadswanson@Rashaads-MacBook-Pro.local>
2015-01-11 20:43:08 -0500

    commit
    commit

--- PWA1-GitHubClone-master/PWA1-January2015/PWA1-Solo_Work/day 3/goal3_debug/js/search.js
+++ PWA1-GitHubClone-master/PWA1-January2015/PWA1-Solo_Work/day 3/goal3_debug/js/search.js
@@ -1,37 +1,41 @@
 // 14 total errors
+//make the function
 (function(){
 	
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
-	var resultsDIV = document.getElementById("results"),
-		searchInput = document.forms[0].search,
-		currentSearch = ''
-	;
+	var resultsDIV = document.getElementById("results");
+		searchInput = document.forms[0].search;
+		currentSearch = '';
 	
 	// Validates search query
-	var validqte == function(query){
+	var validate = function(query){
 		
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){
-			query = query.substring(1, query.length);
+		while(query.charAt(0) === " "){//while loop
+        }
+        // loop through the entire function
+		while(query.charAt(0) === " "){//while loop
+        query = query.substring(1, query.length);
 		};
+         //going through the length of the function
 		while(query.charAt(query.length-1) === ""){
 			query = query.substring(0, query.length-1);
-		;
+            }
 		
 		// Check search length, must have 3 characters
 		if(query.length < 3){
-			alert("Your search query is too small, try again.);
+			alert("Your search query is too small, try again.");//alert the user
 			
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
 			searchInput.focus();
-			return;
+			return;//return the result
 		};
-		
+		// search for the parameter query
 		search(query);
 	};
 	
 	// Finds search matches
-	var search = function(query)
+	var search = function(query){
 		
 		// split the user's search query string into an array
 		var queryArray = query.join(" ");
@@ -50,47 +54,47 @@
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
+				var qitem = queryArray[ii].tolowercase();//allow for lower case inputs
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
 				var compare = dbitem.indexOf(qitem);
-				if(compare !== -1){
-					results.push(db[i]);
+				if(compare !== -1){ //compare function
+					results.push(db[i]);//store results
 				};
 			;
 		;
 		
-		results.sort();
+		results.sort();//sort results
 		
 		// Check that matches were found, and run output functions
 		if(results.length = 0){
-			noMatch();
+			noMatch();//just in case there is no match
 		}else{
-			showMatches(results);
+			showMatches(results);//show results
 		};
 	};
 	
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
 	var noMatch = function(){
-		var html = ''+
-			'<p>No Results found.</p>'+
-			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+		var html = ''+// declare variable
+			'<p>No Results found.</p>'+//just in case no variable is found
+			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'//look for javascript
 		;
-		resultsDIV.innerHTML = html;
+		resultsDIV.innerHTML = html;//store the results
 	};
 	
 	// Put matches into page as paragraphs with anchors
 	var showMatches = function(results){
 		
 		// THE NEXT 4 LINES ARE CORRECT.
-		var html = '<p>Results</p>', 
-			title, 
-			url
+		var html = '<p>Results</p>', //make the html
+			title, //make a title
+			url//make a url
 		;
 		
 		// loop through all the results search() function
-		for(var i=0, j=results.length; i<j; i++){
+		for(var i=0, j=results.length; i<j; i++){//make a for loop in the function
 		
 			// title of video ends with pipe
 			// pull the title's string using index numbers
@@ -109,12 +113,13 @@
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
 	document.forms[0].onsubmit = function(){
-		var query = searchInput.value;
-		validqte(query);
+		var query = searchInput.value;//make variable
+		validate(query); //call the function
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
-		return false;
+		return false;//return the value
 	;
 
 })();
+
